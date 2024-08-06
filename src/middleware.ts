dcbaf5cfcb2a7536b84d79271365fa6595dc3c46
TuanNH <nhtuan0700@gmail.com>
2024-08-06 20:02:07 +0700

    config middleware auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,22 +1,22 @@
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
-  // const authRoutes = ['/login', '/register']
+  const authRoutes = ['/login', '/register']
 
-  // const checkIsAuthRoute = () => {
-  //   return authRoutes.some((route) => {
-  //     return request.nextUrl.pathname.startsWith(route)
-  //   })
-  // }
+  const checkIsAuthRoute = () => {
+    return authRoutes.some((route) => {
+      return request.nextUrl.pathname.startsWith(route)
+    })
+  }
 
-  // const currentUser = request.cookies.get('currentUser')?.value
-  // if (currentUser && checkIsAuthRoute()) {
-  //   return Response.redirect(new URL('/', request.url))
-  // }
+  const currentUser = request.cookies.get('GOLOAD_AUTH')?.value
+  if (currentUser && checkIsAuthRoute()) {
+    return Response.redirect(new URL('/', request.url))
+  }
 
-  // if (!currentUser && !checkIsAuthRoute()) {
-  //   return Response.redirect(new URL('/login', request.url))
-  // }
+  if (!currentUser && !checkIsAuthRoute()) {
+    return Response.redirect(new URL('/login', request.url))
+  }
 }
 
 export const config = {
